6eafeaaa20e17bc20705da9ae22e78903d713ac6
Tom Bishop <tombishop@Toms-MacBook-Pro.local>
2021-07-09 14:43:59 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
+//1 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+//2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// 3
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+// 4
+const wsLink = new WebSocketLink({
+  uri:`ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+// 5
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' && 
+      operation === 'subscription'
+      );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+  );
+
+// 6
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+// 5
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <ApolloProvider client={client}>
+  <App />
+  </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
-);
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
